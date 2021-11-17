93607bd1e356d72bec76627d5f35e551744467b4
Julianny Favinha Donda <julianny.favinha@gmail.com>
2021-11-17 08:44:25 -0300

    Fix iOS 14 issue

--- Feed/FeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ Feed/FeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -17,7 +17,7 @@ class URLSessionHTTPClient {
     }
 
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { _,_, error in
+        session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
             }
@@ -29,7 +29,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     func test_getFromURL_failsOnRequestError() {
         URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "https://any-url.com")!
-        let error = NSError(domain: "any error", code: 1)
+        let error = NSError(domain: "any error", code: 1, userInfo: nil)
         URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
         let sut = URLSessionHTTPClient()
 
@@ -38,7 +38,8 @@ final class URLSessionHTTPClientTests: XCTestCase {
         sut.get(from: url) { result in
             switch result {
             case .failure(let receivedError as NSError):
-                XCTAssertEqual(receivedError, error)
+                XCTAssertEqual(receivedError.domain, error.domain)
+                XCTAssertEqual(receivedError.code, error.code)
             default:
                 XCTFail("Expected failure \(error), got \(result) instead")
             }
@@ -85,10 +86,7 @@ extension URLSessionHTTPClientTests {
         }
 
         override func startLoading() {
-            guard let url = request.url,
-                  let stub = URLProtocolStub.stubs[url] else {
-                return
-            }
+            guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
 
             if let data = stub.data {
                 client?.urlProtocol(self, didLoad: data)
